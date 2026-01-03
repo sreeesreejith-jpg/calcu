db88c269a3775f1088f81720f20da10507507db5
sreeesreejith-jpg <sreee.sreejith@gmail.com>
2026-01-03 10:46:25 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = 'calc-v1';
+const CACHE_NAME = 'calc-v2';
 const ASSETS = [
     './',
     './index.html',
     './style.css',
     './script.js',
-    './manifest.json'
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
